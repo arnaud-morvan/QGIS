9bfbe2aa5715af8023afc616fa4a96a5f076bcce
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-12-18 19:25:34 +0100

    Add QgsFieldsMappingDialog

---
+++ src/gui/qgsfieldsmappingdialog.cpp
@@ -0,0 +1,144 @@
+/***************************************************************************
+   qgsfieldsmappingdialog.cpp
+    --------------------------------------
+   Date                 : December 2016
+   Copyright            : (C) 2016 Arnaud Morvan
+   Email                : arnaud.morvan@gmail.com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "qgsfieldsmappingdialog.h"
+
+QgsFieldsMappingDialog::QgsFieldsMappingDialog( const QgsFields &srcFields, const QgsFields &dstFields, QWidget *parent )
+    : QDialog( parent )
+    , mSrcFields( srcFields )
+    , mDstFields( dstFields )
+    , mModel( this )
+{
+  //init( &mModel );
+}
+
+void QgsFieldsMappingDialog::setMap( const QgsFieldsMapping& map )
+{
+  //removeButton->setEnabled( false );
+  mModel.setMap( map );
+}
+
+///@cond PRIVATE
+void QgsFieldsMappingModel::setMap( const QgsFieldsMapping& map )
+{
+  emit beginResetModel();
+  mLines.clear();
+  for ( QgsFieldsMapping::const_iterator it = map.constBegin(); it != map.constEnd(); ++it )
+  {
+    mLines.append( Line( it.key(), it.value() ) );
+  }
+  emit endResetModel();
+}
+
+QgsFieldsMapping QgsFieldsMappingModel::map() const
+{
+  QgsFieldsMapping ret;
+  for ( QVector<Line>::const_iterator it = mLines.constBegin(); it != mLines.constEnd(); ++it )
+  {
+    if ( !it->first.isEmpty() )
+    {
+      ret[it->first] = it->second;
+    }
+  }
+  return ret;
+}
+
+QgsFieldsMappingModel::QgsFieldsMappingModel( QObject *parent ) :
+    QAbstractTableModel( parent )
+{
+}
+
+int QgsFieldsMappingModel::rowCount( const QModelIndex& parent ) const
+{
+  Q_UNUSED( parent );
+  return mLines.count();
+}
+
+int QgsFieldsMappingModel::columnCount( const QModelIndex & parent ) const
+{
+  Q_UNUSED( parent );
+  return 2;
+}
+
+QVariant QgsFieldsMappingModel::headerData( int section, Qt::Orientation orientation, int role ) const
+{
+  if ( orientation == Qt::Horizontal && role == Qt::DisplayRole )
+  {
+    return QObject::tr( section == 0 ? "Source expression" : "Destination field" );
+  }
+  return QVariant();
+}
+
+QVariant QgsFieldsMappingModel::data( const QModelIndex& index, int role ) const
+{
+  if ( index.row() < 0 ||
+       index.row() >= mLines.count() ||
+       ( role != Qt::DisplayRole && role != Qt::EditRole ) )
+  {
+    return QVariant();
+  }
+  if ( index.column() == 0 )
+    return mLines.at( index.row() ).first;
+  if ( index.column() == 1 )
+    return mLines.at( index.row() ).second.expression();
+  return QVariant();
+}
+
+bool QgsFieldsMappingModel::setData( const QModelIndex & index, const QVariant & value, int role )
+{
+  if ( index.row() < 0 || index.row() >= mLines.count() || role != Qt::EditRole )
+  {
+    return false;
+  }
+  if ( index.column() == 0 )
+  {
+    mLines[index.row()].first = value.toString();
+  }
+  else
+  {
+    //mLines[index.row()].second = value;
+    QgsExpression expression(value.toString());
+    mLines[index.row()].second = expression;
+  }
+  emit dataChanged( index, index );
+  return true;
+}
+
+Qt::ItemFlags QgsFieldsMappingModel::flags( const QModelIndex &index ) const
+{
+  return QAbstractTableModel::flags( index ) | Qt::ItemIsEditable;
+}
+
+bool QgsFieldsMappingModel::insertRows( int position, int rows, const QModelIndex & parent )
+{
+  Q_UNUSED( parent );
+  beginInsertRows( QModelIndex(), position, position + rows - 1 );
+  for ( int i = 0; i < rows; ++i )
+  {
+    mLines.insert( position, Line( QLatin1String( "" ), QgsExpression( QLatin1String( "" ) ) ) );
+  }
+  endInsertRows();
+  return true;
+}
+
+bool QgsFieldsMappingModel::removeRows( int position, int rows, const QModelIndex &parent )
+{
+  Q_UNUSED( parent );
+  beginRemoveRows( QModelIndex(), position, position + rows - 1 );
+  mLines.remove( position, rows );
+  endRemoveRows();
+  return true;
+}
+///@endcond
---
+++ src/gui/qgsfieldsmappingdialog.h
@@ -0,0 +1,94 @@
+/***************************************************************************
+   qgsfieldsmappingdialog.h
+    --------------------------------------
+   Date                 : December 2016
+   Copyright            : (C) 2016 Arnaud Morvan
+   Email                : arnaud.morvan@gmail.com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef QGSFIELDSMAPPINGDIALOG_H
+#define QGSFIELDSMAPPINGDIALOG_H
+
+
+#include <QMap>
+#include <QAbstractTableModel>
+#include <QDialog>
+
+#include "ui_qgsfieldsmappingdialogbase.h"
+#include "qgsfields.h"
+#include "qgsexpression.h"
+
+typedef QMap<QString, QgsExpression> QgsFieldsMapping;
+
+///@cond PRIVATE
+
+/** @ingroup gui
+ * Table model to edit a QVariantMap.
+ * @note added in QGIS 3.0
+ * @note not available in Python bindings
+ */
+class GUI_EXPORT QgsFieldsMappingModel : public QAbstractTableModel
+{
+    Q_OBJECT
+  public:
+
+    explicit QgsFieldsMappingModel( QObject *parent = 0 );
+    void setMap( const QgsFieldsMapping& map );
+    QgsFieldsMapping map() const;
+
+    int rowCount( const QModelIndex& parent = QModelIndex() ) const override;
+    int columnCount( const QModelIndex& parent = QModelIndex() ) const override;
+    QVariant headerData( int section, Qt::Orientation orientation, int role ) const override;
+    QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const override;
+    bool setData( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole ) override;
+    Qt::ItemFlags flags( const QModelIndex &index ) const override;
+    bool insertRows( int position, int rows, const QModelIndex & parent =  QModelIndex() ) override;
+    bool removeRows( int position, int rows, const QModelIndex &parent =  QModelIndex() ) override;
+
+    typedef QPair<QString, QgsExpression> Line;
+
+  private:
+    QVector<Line> mLines;
+};
+///@endcond
+
+/** \ingroup gui
+ * @brief The QgsFieldsMappingDialog class provides a input dialog for mapping between source and destination fields.
+ * @note added in 3.0
+ */
+class GUI_EXPORT QgsFieldsMappingDialog : public QDialog, private Ui::QgsFieldsMappingDialogBase
+{
+    Q_OBJECT
+    Q_PROPERTY( QgsFieldsMapping map READ map WRITE setMap )
+
+  public:
+    /**
+     * Constructor.
+     */
+    explicit QgsFieldsMappingDialog(const QgsFields &srcFields, const QgsFields &dstFields, QWidget *parent = Q_NULLPTR);
+
+    /**
+     * Set the initial value of the widget.
+     */
+    void setMap( const QgsFieldsMapping& map );
+
+    /**
+     * Get the edit value.
+     * @return the QVariantMap
+     */
+    QgsFieldsMapping map() const { return mModel.map(); }
+
+  private:
+    QgsFields mSrcFields;
+    QgsFields mDstFields;
+    QgsFieldsMappingModel mModel;
+};
+
+#endif // QGSFIELDSMAPPINGDIALOG_H
